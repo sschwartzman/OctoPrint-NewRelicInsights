4c53ca4e02eb970ef31fe729d702b8eca0d12a29
Seth Schwartzman <sschwartzman@users.noreply.github.com>
2019-02-12 21:42:13 -0500

    First commit - works!

---
+++ octoprint_insights/__init__.py
@@ -0,0 +1,103 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import json
+import octoprint.plugin
+import requests
+
+class InsightsPlugin(octoprint.plugin.StartupPlugin,
+					octoprint.plugin.SettingsPlugin,
+					octoprint.plugin.ProgressPlugin,
+					octoprint.plugin.EventHandlerPlugin,
+					octoprint.plugin.AssetPlugin,
+					octoprint.plugin.TemplatePlugin):
+
+	def get_settings_defaults(self):
+		return dict(
+            api_urlbase="https://insights-collector.newrelic.com/v1/accounts/",
+            api_urltip="/events",
+            api_inskey="enter_your_insert_key_here",
+            account_id="enter_your_account_id_here",
+            event_type="OctoPrintEvent"
+		)
+
+	def get_assets(self):
+		return dict(
+			js=["js/insights.js"],
+			css=["css/insights.css"],
+			less=["less/insights.less"]
+		)
+
+	def get_update_information(self):
+		return dict(
+			insights=dict(
+				displayName="Insights Plugin",
+				displayVersion=self._plugin_version,
+				type="github_release",
+				user="sschwartzman",
+				repo="OctoPrint-Insights",
+				current=self._plugin_version,
+				pip="https://github.com/sschwartzman/OctoPrint-Insights/archive/{target_version}.zip"
+			)
+		)
+
+	def get_template_configs(self):
+		return [
+			dict(type="settings", custom_bindings=False, name="Insights")
+		]
+
+	def on_settings_save(self, data):
+		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+		self.post_event_to_insights({'message': 'Insights plugin settings saved'})
+		self._logger.info("Insights plugin settings saved.")
+
+	def on_after_startup(self):
+		self.post_event_to_insights({'message': 'Octoprint started'})
+		self._logger.info("Insights plugin started.")
+
+	def on_event(self, event, payload):
+		output = {'message': event}
+		output.update(self.get_print_details())
+		self.post_event_to_insights(output)
+
+	def on_print_progress(self, location, path, progress):
+		output = {'message': 'Print Job Status', 'file': path, 'location': location, 'progress': progress}
+		output.update(self.get_print_details())
+		self.post_event_to_insights(output)
+
+	def on_slicing_progress(self, slicer, source_location, source_path, destination_location, destination_path, progress):
+		pass
+
+	def get_print_details(self):
+		deets = {}
+		deets.update(self._printer.get_current_data())
+		deets.update(self._printer.get_current_job())
+		deets.update(self._printer.get_current_temperatures())
+		deets.update({'state': self._printer.get_state_string()})
+		return deets
+
+	def post_event_to_insights(self, event):
+		event['eventType'] = self._settings.get(['event_type'])
+		event_json = json.dumps([event])
+		headers = {
+			'Content-Type': 'application/json',
+			'X-Insert-Key': self._settings.get(['api_inskey'])
+		}
+		api_fullurl = self._settings.get(['api_urlbase']) + self._settings.get(['account_id']) + self._settings.get(['api_urltip'])
+		response = requests.post(api_fullurl, data=event_json, headers=headers)
+		self._logger.debug(response)
+		success = response.status_code == 200
+		if not success:
+			self._logger.warn(response.content)
+		return success
+
+__plugin_name__ = "Insights Plugin"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = InsightsPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
---
+++ octoprint_insights/static/js/insights.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Insights
+ *
+ * Author: Seth Schwartzman
+ * License: MIT
+ */
+$(function() {
+    function InsightsViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: InsightsViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_insights, #tab_plugin_insights, ...
+        elements: [ /* ... */ ]
+    });
+});
